6df561b287a4b4302bb1e45a90a96af1331fbb42
viitorfernandess <vitorazevedofer007@gmail.com>
2026-06-09 14:50:39 -0300

    feat: adiciona o método findById no repositories

--- src/repositories/customers.repository.js
+++ src/repositories/customers.repository.js
@@ -8,6 +8,15 @@ class CustomersRepository {
 
         return result.rows
     }
+
+    async findById(id) {
+        const result = await pool.query(
+            `SELECT * FROM customers WHERE id = $1`,
+            [id]
+        )
+
+        return result.rows[0]
+    }
 }
 
 export default new CustomersRepository()
