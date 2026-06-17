706cfea1ee49c88874458bfc99b254a73b1aa595
viitorfernandess <vitorazevedofer007@gmail.com>
2026-06-17 16:47:18 -0300

    chore: configura dotenv e ignora arquivos sensíveis

--- database.js
+++ database.js
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import { Pool } from 'pg'
 
 const pool = new Pool({
@@ -5,7 +6,7 @@ const pool = new Pool({
     port: 5432,
     database: 'api-node-postgres',
     user: 'postgres',
-    password: "fernandes21"
+    password: process.env.DB_PASSWORD
 })
 
 export default pool
