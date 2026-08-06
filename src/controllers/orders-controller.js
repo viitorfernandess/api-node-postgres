3227be73a01ed48af6bf2d7840776e5321e7fab4
viitorfernandess <vitorazevedofer007@gmail.com>
2026-08-06 17:31:24 -0300

    refactor implementa AppError no create de orders-controller

--- src/controllers/orders-controller.js
+++ src/controllers/orders-controller.js
@@ -1,37 +1,50 @@
 import ordersRepository from "../repositories/orders-repository.js"
 import customersRepository from "../repositories/customers-repository.js"
+import AppError from "../errors/AppError.js"
 
 class OrdersController {
-    async index(req, res) {
+    async index(req, res, next) {
         const orders = await ordersRepository.findAll()
 
         return res.json(orders)
     }
 
-    async show(req, res) {
-        const { id } = req.params
-        // Regra de negócio
-        const order = await ordersRepository.findById(id)
-        if (!order) {
-            return res.status(404).json({ message: "Order not found" })
-        }
+    async show(req, res, next) {
+        try {
+            const { id } = req.params
 
-        return res.json(order)
+            const order = await ordersRepository.findById(id)
+            if (!order) {
+                throw new AppError("Order not found", 404)
+            }
+
+            return res.json(order)
+        } catch (error) {
+            next(error)
+        }
     }
 
-    async create(req, res) {
-        const { customerId } = req.params
-        const { description, amount } = req.body
+    async create(req, res, next) {
+        try {
+            const { customerId } = req.params
 
-        // Regra de negócio
-        const customer = await customersRepository.findById(customerId)
-        if (!customer) {
-            return res.status(404).json({ message: "Customer not found" })
+            const { description, amount } = req.body
+
+            const customer = await customersRepository.findById(customerId)
+            if (!customer) {
+                throw new AppError("Customer not found", 404)
+            }
+
+            const newOrder = await ordersRepository.create(
+                customerId,
+                description,
+                amount
+            )
+
+            return res.status(201).json(newOrder)
+        } catch (error) {
+            next(error)
         }
-
-        const newOrder = await ordersRepository.create(customerId, description, amount)
-
-        return res.status(201).json(newOrder)
     }
 
     async update(req, res) {
