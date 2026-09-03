32d50bba457effabdec05e7727f3268dba3f64ff
viitorfernandess <vitorazevedofer007@gmail.com>
2026-09-03 14:46:30 -0300

    test: adiciona cenários de exclusão de pedidos

--- tests/orders.controller.test.js
+++ tests/orders.controller.test.js
@@ -335,3 +335,42 @@ test("deve deletar um pedido", async () => {
 
     expect(next).not.toHaveBeenCalled()
 })
+
+test("deve retornar erro quando o pedido não existir", async () => {
+    const spyFindById = jest.spyOn(
+        ordersRepository,
+        "findById"
+    )
+
+    spyFindById.mockResolvedValue(null)
+
+    const spyDelete = jest.spyOn(
+        ordersRepository,
+        "delete"
+    )
+
+    const req = {
+        params: {
+            id: 1
+        }
+    }
+
+    const res = {
+        json: jest.fn()
+    }
+    
+    const next = jest.fn()
+
+    await ordersController.delete(req, res, next)
+
+    expect(next).toHaveBeenCalled()
+
+    const error = next.mock.calls[0][0]
+
+    expect(error.message).toBe("Order not found")
+    expect(error.statusCode).toBe(404)
+
+    expect(spyDelete).not.toHaveBeenCalled()
+
+    expect(res.json).not.toHaveBeenCalled()
+})
