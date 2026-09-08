818d5f2508766f7b1157db6d5438520a86347c2f
viitorfernandess <vitorazevedofer007@gmail.com>
2026-09-08 16:10:51 -0300

    feat: adiciona cadastro de usuarios no repository

--- src/repositories/users-repository.js
+++ src/repositories/users-repository.js
@@ -9,6 +9,21 @@ class UsersRepository {
         )
         return result.rows[0]
     }
+
+    async create(name, email, password) {
+        const result = await pool.query(
+            `INSERT INTO users (
+        name,
+        email,
+        password
+        )
+        
+        VALUES ($1, $2, $3)
+        RETURNING * `,
+            [name, email, password]
+        )
+        return result.rows[0]
+    }
 }
 
 export default new UsersRepository()
