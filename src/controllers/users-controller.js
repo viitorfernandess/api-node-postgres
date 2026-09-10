8bed91430debe520e008d00efda2f0ab77207a02
viitorfernandess <vitorazevedofer007@gmail.com>
2026-09-10 16:26:12 -0300

    feat: implementa auntenticação de usuários com JWT

--- src/controllers/users-controller.js
+++ src/controllers/users-controller.js
@@ -16,6 +16,18 @@ class UsersController {
             next(error)
         }
     }
+
+    async login(req, res, next) {
+        try {
+            const { email, password } = req.body
+
+            const token = await usersService.login(email, password)
+
+            return res.status(200).json(token)
+        } catch (error) {
+            next(error)
+        }
+    }
 }
 
 export default new UsersController()
--- src/routes/users-routes.js
+++ src/routes/users-routes.js
@@ -4,5 +4,6 @@ import usersController from '../controllers/users-controller.js'
 const router = express.Router()
 
 router.post("/users", usersController.create)
+router.post("/login", usersController.login)
 
 export default router
