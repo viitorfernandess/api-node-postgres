9326432f7178043b7e9ce186046e883828f0d083
viitorfernandess <vitorazevedofer007@gmail.com>
2026-09-10 10:57:59 -0300

    feat: implementa login de usuários com JWT

--- src/services/users-service.js
+++ src/services/users-service.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt"
 import usersRepository from "../repositories/users-repository.js"
 import AppError from "../errors/AppError.js"
+import jwt from "jsonwebtoken"
 
 class UsersService {
     async create(name, email, password) {
@@ -17,6 +18,27 @@ class UsersService {
             email,
             hashedPassword
         )
+    }
+
+    async login(email, password) {
+        const user = await usersRepository.findByEmail(email)
+
+        if (!user) {
+            throw new AppError("Invalid email or password", 401)
+        }
+
+        const senhaValida = await bcrypt.compare(password, user.password)
+        if (!senhaValida) {
+            throw new AppError("Invalid email or password", 401)
+        }
+
+        const token = jwt.sign(
+            { id: user.id },
+            process.env.JWT_SECRET,
+            { expiresIn: "1h" }
+        )
+
+        return token
     }
 }
 
