9ca34788f9b27e79570281796fa4abb4a203e71a
viitorfernandess <vitorazevedofer007@gmail.com>
2026-09-21 16:32:59 -0300

    feat: adiciona autorizacao por roles nas rotas de orders

--- src/routes/orders-routes.js
+++ src/routes/orders-routes.js
@@ -2,6 +2,7 @@ import express from 'express'
 import ordersController from '../controllers/orders-controller.js'
 import { validateOrder } from '../middleware/orders-validation-middleware.js'
 import ensureAuth from '../middleware/ensure-auth.js'
+import ensureRole from '../middleware/ensure-role.js'
 
 const router = express.Router()
 
@@ -10,7 +11,7 @@ router.use('/orders', ensureAuth)
 router.get('/orders', ordersController.index)
 router.get('/orders/:id', ordersController.show)
 router.put('/customers/:customerId/orders', ensureAuth, validateOrder, ordersController.create)
-router.put('/orders/:id', validateOrder, ordersController.update)
-router.delete('/orders/:id', ordersController.delete)
+router.put('/orders/:id', ensureRole("admin", "manager"), validateOrder, ordersController.update)
+router.delete('/orders/:id', ensureRole("admin"), ordersController.delete)
 
 export default router
